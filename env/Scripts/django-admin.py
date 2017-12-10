e9858713e9405f138cda9b5f53c600e5c479afcc
Alejandro <alex_gs92@hotmail.com>
2017-12-10 21:00:42 +0100

    Proyecto base para partir propias ramas

--- env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!D:\Caperta personal\Universidad\EGC\Proyecto\GestionVisualizacionDelProgramaEGC1718\env\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
